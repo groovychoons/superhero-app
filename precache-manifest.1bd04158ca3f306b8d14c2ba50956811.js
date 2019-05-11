5f4ed12d255e25eea22ed444c1f2b8ea7ff6d0a4
Zara Siddique <zara@siddique.eu>
2019-05-11 18:18:14 +0100

    Updates

---
+++ precache-manifest.1bd04158ca3f306b8d14c2ba50956811.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "dd0c43ad0c501cd43184c703f385a9a2",
+    "url": "/index.html"
+  },
+  {
+    "revision": "21be27aeb22629fc9762",
+    "url": "/static/css/main.a1181318.chunk.css"
+  },
+  {
+    "revision": "da703b4bfc926ffe484c",
+    "url": "/static/js/2.ebd7e023.chunk.js"
+  },
+  {
+    "revision": "21be27aeb22629fc9762",
+    "url": "/static/js/main.d51b1863.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  },
+  {
+    "revision": "2fc96ddeb0e9cec108d1a2455df5db2a",
+    "url": "/static/media/background.2fc96dde.png"
+  }
+]);
